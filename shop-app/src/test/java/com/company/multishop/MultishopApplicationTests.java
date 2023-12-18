bb30e094862b14c9cd313975d65c7aab8ac87f5e
budarov <budarov@haulmont.com>
2023-12-18 19:39:54 +0400

    project created

---
+++ shop-app/src/test/java/com/company/multishop/MultishopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.company.multishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MultishopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
